1d6e3e4019f9b233f01c69bcd05d23e22110698d
william8697 <scottwilliamsfurnitures@gmail.com>
2025-05-20 01:54:10 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
+const app = require('./app');
+const PORT = process.env.PORT || 3000;
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 const { httpServer } = require('./app');
 const PORT = process.env.PORT || 5000;
 
