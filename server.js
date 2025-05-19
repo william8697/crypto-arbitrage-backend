b1f54a32a6c9056babb6a8b9be364a4f415fa0c2
william8697 <scottwilliamsfurnitures@gmail.com>
2025-05-20 02:10:44 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-// Import the configured app and httpServer from app.js
-const { app, httpServer } = require('./app');
+const { httpServer } = require('./app');  // Destructure only what you need
 const PORT = process.env.PORT || 3000;
 
-// Start the server with Socket.io support
 httpServer.listen(PORT, () => {
   console.log(`Server running with Socket.io on port ${PORT}`);
 });
